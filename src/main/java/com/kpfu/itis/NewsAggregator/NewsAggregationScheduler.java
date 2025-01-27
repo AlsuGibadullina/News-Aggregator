6bb4b3456183e691daea20c3a3eb84886d75b36f
rustemd02 <rustemd02@mail.ru>
2025-01-28 00:57:05 +0300

    NewsAggregationScheduler

---
+++ src/main/java/com/kpfu/itis/NewsAggregator/NewsAggregationScheduler.java
@@ -0,0 +1,21 @@
+package com.kpfu.itis.NewsAggregator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class NewsAggregationScheduler {
+
+    private final NewsAggregationService aggregationService;
+
+    /**
+     * Запускает агрегацию новостей каждые 15 минут.
+     * Cron-выражение: "0 0/15 * * * ?" означает каждую 15-ю минуту каждого часа.
+     */
+    @Scheduled(cron = "0 0/15 * * * ?")
+    public void scheduledAggregation() {
+        aggregationService.aggregateAllNews();
+    }
+}
